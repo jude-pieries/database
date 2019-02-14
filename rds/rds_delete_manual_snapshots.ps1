662e5aec64c2cc39009d0f30d165abfcac234a5c
jude-pieries <31327208+jude-pieries@users.noreply.github.com>
2019-02-14 03:18:50 +0000

    Delete rds_delete_manual_snapshots.ps1

--- rds/rds_delete_manual_snapshots.ps1
+++
@@ -1,8 +0,0 @@
-$20Days = (Get-Date).Adddays(-20) 
-$ManulSonpshot = Get-RDSDBSnapshot -Region us-east-1 | Sort-Object SnapshotCreateTime | select -First 1000 | Where-Object {$_.SnapshotType -eq "manual" -and $_.SnapshotCreateTime -le $20Days } | Select-Object DBInstanceIdentifier , Engine , SnapshotCreateTime ,dbsnapshotidentifier 
-
-foreach ($ManulSonpshot_loop in $ManulSonpshot) 
-{
-  Remove-RDSDBSnapshot -Region us-east-1 -DBSnapshotIdentifier $ManulSonpshot_loop.DBSnapshotIdentifier -Force
-  Write-Host ' Deleted snapshot ' $ManulSonpshot_loop.DBInstanceIdentifier + '-'+ $ManulSonpshot_loop.DBSnapshotIdentifier
-} 
